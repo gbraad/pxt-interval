083aa282d65e33b2cec65fdfedd3f69f52fea674
Gerard Braad <me@gbraad.nl>
2019-12-07 11:36:08 +0000

    Test version

--- main.ts
+++ main.ts
@@ -6,20 +6,26 @@ PXT Interval
 //% color="#00bbff" weight=10 icon="\uf1b9"
 namespace interval {
 
-    let onIntervalHandler: () => void;
+    const myTimerID = 200;
+    const timerTimeout = 1;
+    
+
+    control.onEvent(myTimerID, 0, function () {
+        basic.showNumber(0)
+    })
 
     /**
     * Trigger the execution
     */
-    //% weight=30 blockId="interval_onInterval" block="Execute codeblock on interval"
+    //% weight=30 blockId="interval_onInterval" block="Execute codeblock on interval in ms"
     export function OnInterval(interval: number, cb: () => void) {
-        onInterval(interval, cb)
-    }
-
-
-    //% shim=interval::onInterval
-    function onInterval(interval: number, cb: () => void) {
-        return;
+        //cb
+        control.inBackground(() => {
+            while (true) {
+                basic.pause(interval);
+                control.raiseEvent(myTimerID, timerTimeout);
+            }
+        })
     }
 
 }
